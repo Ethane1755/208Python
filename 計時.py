6bcd61822ea7bfe101b9942624c868db2461facf
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-08-25 12:53:05 +0800

    bug hotfix
    更正未顯示執行時間問題

--- 計時.py
+++ 計時.py
@@ -1,4 +1,4 @@
- import time
+import time
 
 time_start = time.time() #開始計時
 
@@ -6,3 +6,6 @@ time_start = time.time() #開始計時
 #要執行的程式碼，或函式
 
 time_end = time.time()    #結束計時
+
+time_c= time_end - time_start   #執行所花時間
+print('time cost', time_c, 's')
