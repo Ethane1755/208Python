916e724cc489f2b36880c766e36df2dc0fda0cb0
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-08-29 18:14:37 +0800

    html practice

--- Python/計時.py
+++ Python/計時.py
@@ -4,21 +4,6 @@ time_start = time.time() #開始計時
 
 #要執行的程式碼，或函式
 #要執行的程式碼，或函式
-def is_prime(number):
-    for i in range(2,number):
-        if number % i == 0:
-            return False
-    return True
-
-n = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999997
-
-if n ==1:
-    print('不是質數')
-elif is_prime(n):
-    print('是質數')
-else:
-    print('不是質數')
-
 
 time_end = time.time()    #結束計時
 
