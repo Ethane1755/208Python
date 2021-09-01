6d4b1f7dc7aaa2990ecd7d09de397c76c7a0f2cb
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2021-09-01 20:13:38 +0800

    建中附近店面

--- Python/Geocoding/地名2地圖_經緯度輸入.py
+++ Python/Geocoding/地名2地圖_經緯度輸入.py
@@ -5,23 +5,24 @@ import folium
 from folium.plugins import MarkerCluster
 
 locator = Nominatim(user_agent='myGeocoder')
-geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
 
 print('強烈建議使用原文地名')
 a = input('please input the place you want to search:')
+
 locationa = locator.geocode(a)
 lat_lon = locationa.latitude, locationa.longitude
-Map = folium.Map(tiles = 'stamentoner',location=(lat_lon),zoom_start=10) 
-
+Map = folium.Map(location=(lat_lon),zoom_start=10) 
+y = input('please input the name of the place.')
 b = input('Do you want to add another place to the map? ')
 cluster = MarkerCluster().add_to(Map)
-folium.Marker(lat_lon,popup=a).add_to(cluster)
+folium.Marker(lat_lon,popup=y).add_to(cluster)
 
 while b == "yes":
         c = input('please input the place you want to search:')
+        x = input('please input the name of the place.')
         locationc = locator.geocode(c)
         lat_lonc = [locationc.latitude, locationc.longitude]
-        folium.Marker(lat_lonc,popup=c).add_to(cluster)
+        folium.Marker(lat_lonc,popup=x).add_to(cluster)
         b = input('Do you want to add another place to the map? ')  
 else:        
     print('Thank you.')
