bd5af94c02e5f701e49901ab492a1bdd34455193
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-02-08 21:19:20 +0800

    eunion

--- Python/pascal's triangle.py
+++ Python/pascal's triangle.py
@@ -1,16 +1,16 @@
 from math import factorial
- 
+
 n = int(input('please input the layers of your triangle:'))
 for i in range(n):
     for j in range(n-i+1):
- 
+
         # for left spacing
         print(end=" ")
- 
+
     for j in range(i+1):
- 
+
         # nCr = n!/((n-r)!*r!)
         print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
- 
+
     # for new line
     print()
