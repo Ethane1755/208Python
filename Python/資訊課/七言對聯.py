7498642b225d8a210bf121b4eca57cd5648c95fc
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-04-25 22:12:56 +0800

    對聯

---
+++ Python/資訊課/七言對聯.py
@@ -0,0 +1,25 @@
+a=int(input())
+
+c=[]
+for i in range(a):
+    b=[]
+    k=input()
+    m=input()
+    h=k.split()
+    n=m.split()
+    if h[1]!=h[3] and h[1]==h[6] and n[1]!=n[3] and n[1]==n[6]:
+        c.append(1)
+    else:
+        b.append('A')
+    if h[6]=='1' and n[6]=='0':
+        c.append(1)
+    else:
+        b.append('B')
+    if h[1]!=n[1] and h[3]!=n[3] and h[5]!=n[5]:
+        c.append(1)
+    else:
+        b.append('C')
+    if b==[]:
+        print('None')
+    else:
+        print(''.join(b))
