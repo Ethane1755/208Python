73448945773e46ea8acb922ba53cb4b71879f33e
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-06-20 09:29:17 +0800

    format

--- Python/Geocoding/csv to_map.py
+++ Python/Geocoding/csv to_map.py
@@ -2,12 +2,12 @@ import pandas as pd
 import folium
 from folium import plugins
 
-data = pd.read_csv('台灣快篩.csv', encoding='UTF-8',  delimiter=',')
-park_map = folium.Map(
+data=pd.read_csv('台灣快篩.csv', encoding='UTF-8',  delimiter=',')
+map=folium.Map(
     location=[data['Lat'].mean(), data['Long'].mean()],
     zoom_start=8,
     control_scale=True)
-marker_cluster = plugins.MarkerCluster().add_to(park_map)
+marker_cluster=plugins.MarkerCluster().add_to(map)
 
 for name,row in data.iterrows():
     iframe=folium.IFrame('<b><h2>{name}</h2></b><hr><br>{addr}<br>{tele}'.format(
@@ -16,5 +16,6 @@ for name,row in data.iterrows():
             tele=row['診所電話']),)
     folium.Marker(
         location=[row["Lat"], row["Long"]],
-        popup=folium.Popup(iframe,min_width=300, max_width=300),tooltip='{name}'.format(name=row['診所名稱'])).add_to(marker_cluster)
-park_map.save('Taiwan COVID-19 rapid antigen test map.html')
+        popup=folium.Popup(iframe,min_width=300, max_width=300),
+        tooltip='{name}'.format(name=row['診所名稱'])).add_to(marker_cluster)
+map.save('Taiwan COVID-19 Rapid Antigen Test Map.html')
