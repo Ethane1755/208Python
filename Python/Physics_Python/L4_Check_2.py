b5c3338a75560d9ec3f176b3d470e9138b8f6655
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-10-08 17:15:06 +0800

    L41

--- Python/Physics_Python/L4_Check_2.py
+++ Python/Physics_Python/L4_Check_2.py
@@ -4,30 +4,24 @@ from vpython import *
 m1 = 2.0                   #球1質量
 x1 = -20.0                  #球1X軸初位置
 v1= 6.0                    #球1初速度
-size1 = 1.0                 #球1大小
-
+size1 = 2.5
 m2 = 1.0                   #球2質量
 x2 = -5.0                   #球2X軸初位置
 v2= 2.0                    #球2初速度
-size2 = 1.0                 #球2大小
+size2 = 2.5                 #球2大小
 
-spring_k = 5.0                #彈力大小
-spring_L = 5.0             #彈簧長度 
+spring_k = 50              #彈力大小
 
 scene = canvas(width=1000, height=300, background=vec(0.6,0.8,0.8), center=vec(5,0,10),forward=vec(0,0,-1),range=10, fov=0.004)#設定畫面
-ball1 = sphere(radius=size1, color = color.red, make_trail = False)  #設定球1
-ball1.pos = vector(x1,0,0)             #球1位置
-ball1.v = vector (v1,0,0)                        #球1的速度
+ball1 = sphere(radius=size1, color = color.red,opacity = 0.6, make_trail=True, interval=1000)  #設定球1
+ball1.pos = vector(x1,4,0)             #球1位置
+ball1.v = vector (v1,-2,0)                        #球1的速度
 v1_arrow = arrow(pos=ball1.pos,axis=ball1.v,shaftwidth=0.2*size1 ,color = color.red)
 
-ball2 = sphere(radius=size2, color = color.blue,make_trail = False) #設定球2
-ball2.pos = vector(x2,0,0)             #球2位置
+ball2 = sphere(radius=size2, color = color.blue,opacity = 0.6, make_trail=True, interval=1000) #設定球2
+ball2.pos = vector(x2,0,0)             #球2位置,
 ball2.v = vector (v2,0,0)                        #球2的速度
 v2_arrow = arrow(pos=ball2.pos,axis=ball2.v,shaftwidth=0.2*size2 ,color = color.blue)
-
-spring = helix(pos=ball2.pos, radius=0.5, thickness =0.1) #畫彈簧
-spring.coils = 10
-spring.axis = vector(-spring_L,0,0)
 
 t = 0                                            #時間
 dt = 0.001                                       #單位時間
@@ -52,14 +46,16 @@ while True :
     ball1.pos = ball1.pos
     ball2.pos = ball2.pos
     
-    if mag(ball2.pos - ball1.pos) <= spring_L :
-        ball1_a = -1 * spring_k * (spring_L - ( ball2.pos.x - ball1.pos.x )) / m1
-        ball2_a = spring_k * (spring_L - ( ball2.pos.x - ball1.pos.x )) / m2
-        spring.axis = ball1.pos-ball2.pos 
+    if mag(ball2.pos - ball1.pos) <= 5 :
+        ball1.a = -1 * norm(ball2.pos-ball1.pos) * spring_k * (5 - mag( ball2.pos - ball1.pos )) / m1
+        ball2.a = norm(ball2.pos-ball1.pos) * spring_k * (5 - mag( ball2.pos - ball1.pos )) / m1
     else :                             #如果沒有，兩球的加速度均為0
-        ball1_a = 0
-        ball2_a = 0
-        spring.axis = vector(-spring_L,0,0)
+        ball1.a = vec(0,0,0)
+        ball2.a = vec(0,0,0)
+    if t>4.0 and t< 4.1: #注意此必須在碰撞結束後才計算角度
+        cos_theta = dot(ball1.v,ball2.v)/(ball1.v.mag*ball2.v.mag)
+        theta = acos(cos_theta)*360/(2*pi) #將弧度改為角度
+        print (theta)
 
     t=t+dt
     ball1.pos = ball1.pos+ball1.v*dt  #控制球1的運動
@@ -70,24 +66,7 @@ while True :
 
     v2_arrow.pos = ball2.pos #球2速度向量箭頭的起始點在球2上
     v2_arrow.axis = ball2.v  #球1速度向量箭頭的長度與方向等於球2速度
-
-    spring.pos=ball2.pos  #彈簧的起始點位置在球2上
-    
-    K = 0.5*(m1 * (mag(ball1.v)**2))+0.5*(m2 * (mag(ball2.v)**2))
-    if mag(ball2.pos - ball1.pos) >= spring_L :
-        U=0
-    else :                             #如果沒
-        U = 0.5 * spring_k * (spring_L-(ball2.pos.x-ball1.pos.x))**2
-    F = K+U
-
-    ball1.v = ball1.v + vector(ball1_a,0,0) *dt  #加速度是向量，所以要用vector(ball1_a,0,0)
-    ball2.v = ball2.v + vector(ball2_a,0,0) *dt
-    f1_1.plot( pos=(t,K))
-    f1_2.plot( pos=(t,U))
-    f1_3.plot( pos=(t,F))
-    P1 = m1*ball1.v.x
-    P2 = m2*ball2.v.x
-    P = P1+P2
-    f2_1.plot( pos=(t,P1))
-    f2_2.plot( pos=(t,P2))
-    f2_3.plot( pos=(t,P))
+    k=ball1.a*dt
+    k1=ball2.a*dt
+    ball1.v = ball1.v + k #加速度是向量，所以要用vector(ball1_a,0,0)
+    ball2.v = ball2.v + k1
