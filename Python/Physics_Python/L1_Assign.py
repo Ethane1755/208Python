622b7dbc7c744ca9d294e845a1fba95680f64cca
Ethane1755 <87981364+Ethane1755@users.noreply.github.com>
2022-11-06 22:39:16 +0800

    shift_alt_F?

--- Python/Physics_Python/L1_Assign.py
+++ Python/Physics_Python/L1_Assign.py
@@ -2,35 +2,39 @@ from vpython import *
 
 size = 0.1
 
-scene = canvas(width=600, height=400, center=vector(2.5,0,0), background=vector(0,0,0))
+scene = canvas(width=600, height=400, center=vector(
+    2.5, 0, 0), background=vector(0, 0, 0))
 
-x = arrow(pos=vector(0,0,0), axis=vector(1,0,0), shaftwidth=0.02, color=color.green)
-y = arrow(pos=vector(0,0,0), axis=vector(0,1,0), shaftwidth=0.02, color=color.red)
-z = arrow(pos=vector(0,0,0), axis=vector(0,0,1), shaftwidth=0.02, color=color.blue)
-ball = sphere(radius=size, color=color.yellow, pos=vector(0,0,0), v=vector(-2.0,0,0))
+x = arrow(pos=vector(0, 0, 0), axis=vector(
+    1, 0, 0), shaftwidth=0.02, color=color.green)
+y = arrow(pos=vector(0, 0, 0), axis=vector(
+    0, 1, 0), shaftwidth=0.02, color=color.red)
+z = arrow(pos=vector(0, 0, 0), axis=vector(
+    0, 0, 1), shaftwidth=0.02, color=color.blue)
+ball = sphere(radius=size, color=color.yellow,pos=vector(0, 0, 0), v=vector(-2.0, 0, 0))
 
-gd = graph(title = "x-t plot", width=600, height=400, xtitle="t", ytitle="x")
-f1 = gcurve(color=color.blue)  
-gd1 = graph(title = "v-t plot", width=600, height=400, xtitle="t", ytitle="x")
+gd = graph(title="x-t plot", width=600, height=400, xtitle="t", ytitle="x")
+f1 = gcurve(color=color.blue)
+gd1 = graph(title="v-t plot", width=600, height=400, xtitle="t", ytitle="x")
 f2 = gcurve(color=color.green)
-gd3 = graph(title = "a-t plot", width=600, height=400, xtitle="t", ytitle="x")
+gd3 = graph(title="a-t plot", width=600, height=400, xtitle="t", ytitle="x")
 f3 = gcurve(color=color.red)
 
 dt = 0.001
 t = 0.0
-v=vector(-2.0,0,0)
-a=vector(3.0,0,0)
+v = vector(-2.0, 0, 0)
+a = vector(3.0, 0, 0)
 
-while t<3:
+while t < 3:
     rate(1/dt)
     t = t+dt
     ball.pos = ball.pos + (v+a*t)*dt
     ball.v = ball.v + a*dt
     ball.a = a
-    f1.plot(pos=(t,ball.pos.x))
-    f2.plot(pos=(t,ball.v.x))
-    f3.plot(pos=(t,ball.a.x))
+    f1.plot(pos=(t, ball.pos.x))
+    f2.plot(pos=(t, ball.v.x))
+    f3.plot(pos=(t, ball.a.x))
     if ball.v.x < 0 and ball.v.x + ball.a.x*dt > 0:
-        print('time:',t)
-        print('position:',ball.pos.x)
-        print('velocity:',ball.v.x)
+        print('time:', t)
+        print('position:', ball.pos.x)
+        print('velocity:', ball.v.x)
